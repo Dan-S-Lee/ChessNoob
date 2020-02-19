28de356936d97b10055ef4180368f52c33f475ff
SleepingTuna <34869440+SleepingTuna@users.noreply.github.com>
2020-02-18 22:55:28 -0500

    Update df_processing.py

--- df_processing.py
+++ df_processing.py
@@ -21,13 +21,25 @@ board_array = np.array(board_list)
 white_list = ['p', 'n', 'b', 'r', 'q', 'k']
 black_list = [l.upper() for l in white_list]
 
-white_dict = {}
-black_dict = {}
+white_list.extend(black_list)
+piece_list = white_list
 
-for i in range(0, len(white_list)):
-    white_row = [0] * 6
-    black_row = [0] * 6
-    white_row[i] = 1
-    black_row[i] = -1
-    white_dict[white_list[i]] = white_row
-    black_dict[black_list[i]] = black_row
+piece_dict = {}
+
+for i in range(0, 6):
+    piece_dict[piece_list[i]] = i
+    piece_dict[piece_list[i+6]] = i
+
+for square in chess.SQUARES:
+    square_piece = board.piece_at(square = square)
+    if square_piece:
+        piece_sym = square_piece.symbol()
+        c = square_piece.color
+        print('Square int - {}'.format(square))
+        print('Row - {}'.format(square//8))
+        print('Column - {}'.format(square%8))
+        board_array[piece_dict[piece_sym], 
+                    square//8, 
+                    square%8] = 2 * int(c) - 1
+                    
+print(board_array)
