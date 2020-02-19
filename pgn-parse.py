33855be1bb074b99c9ee91af3b51e50b6e38dc0c
SleepingTuna <34869440+SleepingTuna@users.noreply.github.com>
2020-02-18 22:00:16 -0500

    Update pgn-parse.py
    Added Results header to dataframe

--- pgn-parse.py
+++ pgn-parse.py
@@ -10,16 +10,17 @@ import chess.pgn
 import numpy as np
 import pandas as pd
 
-game_text = open('data/ficsgamesdb_2019_standard2000_nomovetimes_115478.pgn')
+game_text = open('data/ficsgamesdb_2019_standard2000_nomovetimes_115366.pgn')
 game_dict = {}
 i = 0
-game_df = pd.DataFrame(columns = ['Game', 'Moves'])
+game_df = pd.DataFrame(columns = ['Game', 'Moves', 'Result'])
 while True:
     game = chess.pgn.read_game(game_text)
     if game is None: break
     game_line = []
     game_line.append('Game{}'.format(i))
     game_line.append(game.board().variation_san(game.mainline_moves()))
+    game_line.append(game.headers['Result'])
     game_df.loc[len(game_df)] = game_line
     i+=1
 game_df.to_csv('2019_df.csv')
