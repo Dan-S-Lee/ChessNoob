b717e0161e4e2295f587713198bd100d9b584ae9
SleepingTuna <34869440+SleepingTuna@users.noreply.github.com>
2020-02-19 13:10:19 -0500

    Update pgn-parse.py
    Added split functionality to string to stop using chess module to parse pgn. 
    
    Added dynamic file naming, will wrap in function later once pre-processing is finished.

--- pgn-parse.py
+++ pgn-parse.py
@@ -9,18 +9,26 @@ import chess
 import chess.pgn
 import numpy as np
 import pandas as pd
+import os
 
-game_text = open('data/ficsgamesdb_2019_standard2000_nomovetimes_115366.pgn')
+data_dir = 'data/'
+file_list = [f for f in os.listdir(data_dir)]
+
+game_text = open(data_dir + file_list[0]).read()
 game_dict = {}
 i = 0
-game_df = pd.DataFrame(columns = ['Game', 'Moves', 'Result'])
-while True:
-    game = chess.pgn.read_game(game_text)
-    if game is None: break
-    game_line = []
-    game_line.append('Game{}'.format(i))
-    game_line.append(game.board().variation_san(game.mainline_moves()))
-    game_line.append(game.headers['Result'])
-    game_df.loc[len(game_df)] = game_line
-    i+=1
-game_df.to_csv('2019_df.csv')
+
+game_list = game_text.split('\n\n')
+
+headers_list = game_list[::2]
+move_list = game_list[1::2]
+
+if len(headers_list[len(headers_list)-1]) < 3:
+    headers_list = headers_list[:-1] # last item of list is blank for some reason
+
+df_dict = {'headers': headers_list, 'moves': move_list}
+
+game_df = pd.DataFrame(df_dict)
+
+game_df['result'] = game_df['moves'].apply(lambda x: x.split(' ')[-1])
+game_df.to_csv(file_list[0].split('_')[1] + '_df.csv')
